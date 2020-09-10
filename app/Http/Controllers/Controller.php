15259628e8f9183d4ad0fcd077187ed477cf436d
thanatos <thanatos915@163.com>
2020-09-10 17:55:26 +0800

    BUG

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use Dingo\Api\Routing\Helpers;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
@@ -9,5 +10,16 @@ use Illuminate\Routing\Controller as BaseController;
 
 class Controller extends BaseController
 {
-    use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+    use AuthorizesRequests, DispatchesJobs, ValidatesRequests, Helpers;
+
+    /**
+     * 返回数组
+     * @param $data
+     * @return mixed
+     * @author thanatos thanatos915@163.com
+     */
+    public function resArray($data)
+    {
+        return $this->response->array(['data' => $data]);
+    }
 }
